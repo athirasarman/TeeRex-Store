3fc123d9dc988a97ab9ecebcf2167aa88075d976
athirasarman <kaithakkottuathira@gmail.com>
2022-12-13 13:46:54 +0530

    Folder structure change and scss fixes

--- store/src/app/app-routing.module.ts
+++ store/src/app/app-routing.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
-  { path: 'cart', loadChildren: () => import('./cart/cart.module').then(m => m.CartModule) },
-  { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) }
+  { path: 'products', loadChildren: () => import('./modules/products/products.module').then(m => m.ProductsModule) },
+  { path: 'cart', loadChildren: () => import('./modules/cart/cart.module').then(m => m.CartModule) },
+  { path: 'shared', loadChildren: () => import('./modules/shared/shared.module').then(m => m.SharedModule) }
 ];
 
 @NgModule({
